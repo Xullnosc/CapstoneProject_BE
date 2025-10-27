4f3857a83c94503d27dfb14dc7393b627a292d55
Derit83 <155723417+Derit83@users.noreply.github.com>
2025-10-27 22:11:06 +0700

    Remove environment variables

--- powershell/environment-variables.ps1
+++
@@ -1,4 +0,0 @@
-$devConfigPath = Join-Path $PSScriptRoot "..\CapstoneProject_BE\appsettings.Development.json"
-$defaultConfigPath = Join-Path $PSScriptRoot "..\CapstoneProject_BE\appsettings.json"
-$modelsFolder = Join-Path $PSScriptRoot "..\BusinessObjects\Models"
-$settingsPath = Join-Path $PSScriptRoot "..\CapstoneProject_BE\appsettings.Development.json"
