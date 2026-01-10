9ca156dfa466f3976340b137bd0ffb2e5b7de45d
Chuong <chuongww23@gmail.com>
2026-01-10 18:22:04 +0700

    Added new packages

--- powershell/install-packages.ps1
+++ powershell/install-packages.ps1
@@ -5,12 +5,15 @@ $packages = @(
     "Microsoft.EntityFrameworkCore.SqlServer",
     "Microsoft.EntityFrameworkCore.Design",
     "Microsoft.Extensions.Configuration",
-    "Microsoft.Extensions.Configuration.Json"
+    "Microsoft.Extensions.Configuration.Json",
+    "Microsoft.EntityFrameworkCore.Tools"
 )
-
 foreach ($pkg in $packages) {
     Write-Host "Installing $pkg..."
     dotnet add package $pkg --version 9.0.6
 }
+dotnet add package AutoMapper --version 15.0.1
+dotnet add package Google.Apis.Auth --version 1.73.0
+dotnet add package Microsoft.AspNetCore.Authentication.JwtBearer --version 8.0.0
 
 Write-Host "installed Nuget packages"
