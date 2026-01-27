c3659c23856c7f2378e22bdeba70c5191067542c
Ngoc Son <ncns924@gmail.com>
2026-01-27 12:03:21 +0700

    Update: "Thêm Log vào AuthService + Cấu hình lại CORS"

--- CapstoneProject_BE/Program.cs
+++ CapstoneProject_BE/Program.cs
@@ -58,14 +58,19 @@ builder.Services.AddSwaggerGen(c =>
 });
 builder.Services.AddHttpClient();
 
-builder.Services.AddCors(options =>
+var allowedOrigins = builder.Configuration["AllowedOrigins"];
+
+if (!string.IsNullOrEmpty(allowedOrigins))
 {
-    options.AddPolicy("AllowReactApp",
-        builder => builder
-            .WithOrigins("http://localhost:5173")
-            .AllowAnyMethod()
-            .AllowAnyHeader());
-});
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowReactApp",
+            builder => builder
+                .WithOrigins(allowedOrigins.Split(',', StringSplitOptions.RemoveEmptyEntries))
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+    });
+}
 
 // Dependency injection
 builder.Services.AddScoped<IAuthService, AuthService>();
