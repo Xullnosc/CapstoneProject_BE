5f731ed96afc95d548be21b6a6ea115dbf187f37
Tran Dinh Binh Minh <kunshtran@gmail.com>
2026-03-03 19:14:03 +0700

    Cancel Proposal

--- Services/IThesisService.cs
+++ Services/IThesisService.cs
@@ -17,6 +17,7 @@ namespace Services
 
         // Phase 02: New methods
         Task<ThesisDTO> UpdateThesisAsync(string thesisId, UpdateThesisDTO req, string email);
+        Task<ThesisDTO> CancelThesisAsync(string thesisId, string email);
         Task<IEnumerable<ThesisDTO>> GetMyThesesAsync(string email, string? status = null, string? searchTitle = null);
         Task<ThesisDTO?> GetThesisDetailAsync(string id);
         Task<IEnumerable<ThesisDTO>> GetFilteredThesesAsync(string? status, int? userId, string? searchTitle = null);
